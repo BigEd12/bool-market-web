f15984d77879d0bb7e8514247448d31a6a8b3300
Eduardo Olmos <olmos.edward@gmail.com>
2023-09-06 12:00:51 +0200

    candlestick chart added

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template, url_for, redirect, request, jsonify
 from flask_bootstrap import Bootstrap
+
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import json
+import plotly
 
 from wtforms import Form, StringField
 from wtforms.validators import InputRequired
@@ -27,20 +33,27 @@ def index():
 
             # candlestick_data = fetch_candlestick_data(candle_ticker, from_date, to_date)
             # candlestick_data = process_candlestick_data(candlestick_data)
-
-    initial_candlestick_data = {
-        'x': [1, 2, 3, 4, 5],
-        'open': [100, 100, 100, 120, 150],
-        'high': [110, 110, 110, 130, 160],
-        'low': [90, 90, 90, 90, 110],
-        'close': [105, 105, 105, 125, 140],
-    }
-
+    
+    df = pd.DataFrame({
+      'x': ['1', '2', '3', '4', '5'],
+      'open': [20, 25, 30, 35, 40],
+      'low': [15, 20, 25, 30, 35],
+      'high': [25, 30, 35, 40, 45],
+      'close': [22, 27, 32, 37, 42]
+    })
+    fig = go.Figure(data=[go.Candlestick(x=df['x'],
+                open=df['open'],
+                high=df['high'],
+                low=df['low'],
+                close=df['close'])])
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
     return render_template('index.html',
                            candle_form=candle_form,
                            pattern_form=pattern_form,
+                           graphJSON=graphJSON
                            )
+
 
 
 if __name__ == "__main__":
